7d41cfdc9765fe22fb52e29e6d3146ff2b0e71f0
Frederico Silva <fredericochopin@gmail.com>
2020-01-26 14:24:50 -0300

    Initial commit

---
+++ src/main/java/br/com/fred/aopintercept/AopInterceptApplication.java
@@ -0,0 +1,13 @@
+package br.com.fred.aopintercept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopInterceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopInterceptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fred/aopintercept/AopInterceptApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fred.aopintercept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopInterceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
